9badcfc4da2671016828a3aa1bc48e63a8b41831
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-05-15 21:07:33 -0400

    Update daily_signals.py

--- pages/daily_signals.py
+++ pages/daily_signals.py
@@ -22,7 +22,7 @@ def seasonals_chart(tick):
 	plot_ytd="Yes"
 	all_=""
 	end_date=dt.datetime(2022,12,30)
-	this_yr_end=dt.datetime(2023,5,10)
+	this_yr_end=dt.datetime.now().date()
 
 
 	spx1=yf.Ticker(ticker)
