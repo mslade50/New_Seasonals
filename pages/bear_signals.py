b66940833bb6bbc176bc312d0bb919a2aa38eb3c
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-05-16 08:08:38 -0400

    Update bear_signals.py

--- pages/bear_signals.py
+++ pages/bear_signals.py
@@ -567,35 +567,27 @@ def seasonals_chart(tick):
 	    annotations=annotations  # Use the new annotations list with colored text
 	)
 	# Create a candlestick chart
-	fig2 = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.03)
+	fig2 = go.Figure()
 
-	fig2.add_trace(go.Scatter(x=df.index, y=df['RSI'], name='RSI'), row=1, col=1)
+	# Add only the Price (Candlestick) trace and the 200_MA trace
 	fig2.add_trace(go.Candlestick(x=df['date_str'],
-				      open=df['Open'],
-				      high=df['High'],
-				      low=df['Low'],
-				      close=df['Close'], name='Price'), row=2, col=1)
+				     open=df['Open'],
+				     high=df['High'],
+				     low=df['Low'],
+				     close=df['Close'], name='Price'))
 
-	fig2.add_trace(go.Scatter(x=df['date_str'], y=df['200_MA'], name='200_MA', line=dict(color='purple')), row=2, col=1)
+	fig2.add_trace(go.Scatter(x=df['date_str'], y=df['200_MA'], name='200_MA', line=dict(color='purple')))
 
-	# Adjust the domain of y-axes for the height ratio
-	fig2['layout']['yaxis1'].update(domain=[0.875, 1])
-	fig2['layout']['yaxis2'].update(domain=[0, 0.875], showgrid=False)
-
-	# Add horizontal lines at RSI=70 and RSI=30
+	# Update layout
 	fig2.update_layout(
-	    shapes=[
-		dict(type='line', yref='y1', y0=70, y1=70, xref='x', x0=df.index[0], x1=df.index[-1], line=dict(color='Red')),
-		dict(type='line', yref='y1', y0=30, y1=30, xref='x', x0=df.index[0], x1=df.index[-1], line=dict(color='Green'))
-	    ],
 	    title=ticker, # Replace ticker with your ticker variable
 	    title_x=0.5, # This will center the title
 	    height=800, 
-	    width=1200
+	    width=1200,
+	    xaxis=dict(showgrid=False),  # Remove grid lines
+	    yaxis=dict(showgrid=False),  # Remove grid lines
 	)
 
-	fig2.update_xaxes(showgrid=False)
-	fig2.update_yaxes(showgrid=False)
 
 	st.plotly_chart(fig)
 	st.plotly_chart(fig2)
