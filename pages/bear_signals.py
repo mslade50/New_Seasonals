ccf6d62ce59eda68bae825ded37f62000db588f0
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-05-16 07:42:18 -0400

    Update bear_signals.py

--- pages/bear_signals.py
+++ pages/bear_signals.py
@@ -22,12 +22,11 @@ def seasonals_chart(tick):
 	plot_ytd="Yes"
 	all_=""
 	end_date=dt.datetime(2022,12,30)
-	this_yr_end=dt.datetime(2023,5,10)
+	this_yr_end=dt.datetime.today()
 
 
 	spx1=yf.Ticker(ticker)
 	spx = spx1.history(period="max",end=end_date)
-	start_year = spx.index.min().year
 	df= spx1.history(period="max",end=this_yr_end)
 	df['200_MA'] = df['Close'].rolling(window=200).mean()
 	df['RSI'] = RSIIndicator(df['Close']).rsi()
@@ -446,161 +445,161 @@ def seasonals_chart(tick):
 	dfy=pd.DataFrame(yr_master)
 	dfy1=dfy.mean()
 	s3=dfy1.cumsum()
-	dfy=pd.DataFrame(yr_master)
-	dfy1=dfy.mean()
-	s3=dfy1.cumsum()
 	##Mean Return paths chart (looks like a classic 'seasonality' chart)
 	# plot2=plt.figure(2)
-	fig = go.Figure()
-	if start_year > 2018:
-	    series_to_plot = s3
-	    y_max = max(s3.max(), days2['this_yr'].max()) if plot_ytd == 'Yes' else s3.max()
-	    y_min = min(s3.min(),days2['this_yr'].min(),0)
-	    y_value_at_length = np.interp(length_value, s3.index, s3.values)
-	    series_values = s3.values[:length]
-	else:
-	    series_to_plot = s4
-	    y_max = max(s4.max(), days2['this_yr'].max()) if plot_ytd == 'Yes' else s4.max()
-	    y_min = min(s4.min(),days2['this_yr'].min(),0)
-	    y_value_at_length = np.interp(length_value, s4.index, s4.values)
-	    series_values = s4.values[:length]
+	if spx.index.min().year < 2018:
+		fig = go.Figure()
 
-	fig.add_trace(go.Scatter(x=series_to_plot.index, y=series_to_plot.values, mode='lines', name=cycle_label, line=dict(color='orange')))
-	if plot_ytd == 'Yes':
-	    fig.add_trace(go.Scatter(x=days2.index, y=days2['this_yr'], mode='lines', name='Year to Date', line=dict(color='green')))
-# 	y1 = max(s4.max(), days2['this_yr'].max()) if plot_ytd == 'Yes' else s4.max()
-# 	y0=min(s4.min(),days2['this_yr'].min(),0)
-	# Assuming 'length' variable is defined and within the range of the x-axis
-	length_value = length
+		fig.add_trace(go.Scatter(x=s4.index, y=s4.values, mode='lines', name=cycle_label, line=dict(color='orange')))
+		if plot_ytd == 'Yes':
+		    fig.add_trace(go.Scatter(x=days2.index, y=days2['this_yr'], mode='lines', name='Year to Date', line=dict(color='green')))
+		y1 = max(s4.max(), days2['this_yr'].max()) if plot_ytd == 'Yes' else s4.max()
+		y0=min(s4.min(),days2['this_yr'].min(),0)
+		# Assuming 'length' variable is defined and within the range of the x-axis
+		length_value = length
 
-	# Interpolate Y value at the specified X coordinate
-	y_value_at_length = np.interp(length_value, series_to_plot.index, series_to_plot.values)
-	series_values = series_to_plot.values[:length]
+		# Interpolate Y value at the specified X coordinate
+		y_value_at_length = np.interp(length_value, s4.index, s4.values)
+		s4_values = s4.values[:length]
+		this_year_values = days2['this_yr'][:length]
+		this_year_values = np.where(np.isnan(this_year_values), np.nanmean(this_year_values), this_year_values)
 
-	this_year_values = days2['this_yr'][:length]
-	this_year_values = np.where(np.isnan(this_year_values), np.nanmean(this_year_values), this_year_values)
+		if np.isnan(s4_values).any() or np.isnan(this_year_values).any() or np.var(s4_values) == 0 or np.var(this_year_values) == 0:
+		    correlation_coefficient = 'N/A'
+		else:
+		    correlation_matrix = np.corrcoef(s4_values, this_year_values)
+		    correlation_coefficient = correlation_matrix[0, 1]
+		    correlation_coefficient = f"{correlation_coefficient:.2f}"
+		def sign_agreement(a, b, window):
+		    a_changes = a[window:] - a[:-window]
+		    b_changes = b[window:] - b[:-window]
+		    return np.mean(np.sign(a_changes) == np.sign(b_changes))
 
-	if np.isnan(series_values).any() or np.isnan(this_year_values).any() or np.var(series_values) == 0 or np.var(this_year_values) == 0:
-	    correlation_coefficient = 'N/A'
-	else:
-	    correlation_matrix = np.corrcoef(series_values, this_year_values)
-	    correlation_coefficient = correlation_matrix[0, 1]
-	    correlation_coefficient = f"{correlation_coefficient:.2f}"
+	# 	correlations = []
+	# 	window_size = 5
 
-	def sign_agreement(a, b, window):
-	    a_changes = a[window:] - a[:-window]
-	    b_changes = b[window:] - b[:-window]
-	    return np.mean(np.sign(a_changes) == np.sign(b_changes))
+	# 	for i in range(len(s4_values) - window_size + 1):
+	# 		window_s4 = s4_values[i : i + window_size]
+	# 		window_this_year = this_year_values[i : i + window_size]
+	# 		if np.isnan(window_s4).any() or np.isnan(window_this_year).any() or np.var(window_s4) == 0 or np.var(window_this_year) == 0:
+	# 			correlations.append(np.nan)
+	# 		else:
+	# 			correlation_matrix = np.corrcoef(window_s4, window_this_year)
+	# 			correlation_coefficient = correlation_matrix[0, 1]
+	# 			correlations.append(correlation_coefficient)
 
-	# Calculate sign agreement for 5-day, 10-day, and 21-day forward changes
-	sign_agreement_1d = sign_agreement(s4_values, this_year_values, window=1).round(2)
-	sign_agreement_5d = sign_agreement(s4_values, this_year_values, window=5).round(2)
-	sign_agreement_10d = sign_agreement(s4_values, this_year_values, window=10).round(2)
-	sign_agreement_21d = sign_agreement(s4_values, this_year_values, window=21).round(2)
-	# Add a white dot at the specified X coordinate and the interpolated Y value
-	fig.add_trace(go.Scatter(x=[length_value], y=[y_value_at_length], mode='markers', marker=dict(color='white', size=8), name='White Dot' ,showlegend=False))
-	def text_color(value, reverse=False):
-	    if not reverse:
-	        if value >= 85:
-	            return 'green'
-	        elif value <= 15:
-	            return 'red'
-	        else:
-	            return 'white'
-	    else:
-	        if value >= 85:
-	            return 'red'
-	        elif value <= 15:
-	            return 'green'
-	        else:
-	            return 'white'
-	def create_annotation(x, y, text, color):
-	    return dict(
-		x=x,
-		y=y,
-		xref='paper',
-		yref='paper',
-		text=text,
-		showarrow=False,
-		font=dict(size=12, color=color),
-		bgcolor='rgba(0, 0, 0, 0.5)',
-		bordercolor='grey',
-		borderwidth=1,
-		borderpad=4,
-		align='left'
-	    )
+	# 	average_correlation = pd.Series(correlations).mean()
+	# 	average_correlation = f"{average_correlation:.2f}"
 
-	annotations = [
-	    create_annotation(0.4, -0.22, f"Cycle Avg: {cycle_avg}", text_color(cycle_avg)),
-	    create_annotation(0.55, -0.22, f"Total Avg: {total_avg}", text_color(total_avg)),
-	    create_annotation(0.85, -0.22, f"Trailing 21 Rank: {trailing_21_rank}", text_color(trailing_21_rank, reverse=True)),
-	    create_annotation(1.04, -0.22, f"Trailing 5 Rank: {trailing_5_rank}", text_color(trailing_5_rank, reverse=True)),
-	]
-	annotations.append(
-	    create_annotation(
-		1.02,
-		1.10,
-		f"5d and 21d Concordance: {sign_agreement_5d}, {sign_agreement_21d}",
-		'white'
-	    )
-	)
-# 	annotations.append(create_annotation(0.95, 1.12, f"Average 14-Period Rolling Correlation: {average_correlation}", 'white'))
-	fig.update_layout(
-	    title=f"Mean return path for {ticker2} in years {start}-present",
-	    legend=dict(
-		bgcolor='rgba(0,0,0,0)',
-		font=dict(color='White'),
-		itemclick='toggleothers',
-		itemdoubleclick='toggle',
-		traceorder='reversed',
-		orientation='h',
-		bordercolor='grey',
-		borderwidth=1,
-		x=-0.10,
-		y=-0.135 
-	    ),
-	    xaxis=dict(title='', color='white',showgrid=False),
-	    yaxis=dict(title='Mean Return', color='white',showgrid=False),
-	    font=dict(color='white'),
-	    margin=dict(l=40, r=40, t=40, b=70),  # Increase bottom margin
-	    hovermode='x',
-	    plot_bgcolor='Black',
-	    paper_bgcolor='Black',
-	    annotations=annotations  # Use the new annotations list with colored text
-	)
-	# Create a candlestick chart
-	fig2 = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.03)
+		# Calculate sign agreement for 5-day, 10-day, and 21-day forward changes
+		sign_agreement_1d = sign_agreement(s4_values, this_year_values, window=1).round(2)
+		sign_agreement_5d = sign_agreement(s4_values, this_year_values, window=5).round(2)
+		sign_agreement_10d = sign_agreement(s4_values, this_year_values, window=10).round(2)
+		sign_agreement_21d = sign_agreement(s4_values, this_year_values, window=21).round(2)
+		# Add a white dot at the specified X coordinate and the interpolated Y value
+		fig.add_trace(go.Scatter(x=[length_value], y=[y_value_at_length], mode='markers', marker=dict(color='white', size=8), name='White Dot' ,showlegend=False))
+		def text_color(value, reverse=False):
+		    if not reverse:
+			if value >= 85:
+			    return 'green'
+			elif value <= 15:
+			    return 'red'
+			else:
+			    return 'white'
+		    else:
+			if value >= 85:
+			    return 'red'
+			elif value <= 15:
+			    return 'green'
+			else:
+			    return 'white'
+		def create_annotation(x, y, text, color):
+		    return dict(
+			x=x,
+			y=y,
+			xref='paper',
+			yref='paper',
+			text=text,
+			showarrow=False,
+			font=dict(size=12, color=color),
+			bgcolor='rgba(0, 0, 0, 0.5)',
+			bordercolor='grey',
+			borderwidth=1,
+			borderpad=4,
+			align='left'
+		    )
 
-	fig2.add_trace(go.Scatter(x=df.index, y=df['RSI'], name='RSI'), row=1, col=1)
-	fig2.add_trace(go.Candlestick(x=df['date_str'],
-				      open=df['Open'],
-				      high=df['High'],
-				      low=df['Low'],
-				      close=df['Close'], name='Price'), row=2, col=1)
+		annotations = [
+		    create_annotation(0.4, -0.22, f"Cycle Avg: {cycle_avg}", text_color(cycle_avg)),
+		    create_annotation(0.55, -0.22, f"Total Avg: {total_avg}", text_color(total_avg)),
+		    create_annotation(0.85, -0.22, f"Trailing 21 Rank: {trailing_21_rank}", text_color(trailing_21_rank, reverse=True)),
+		    create_annotation(1.04, -0.22, f"Trailing 5 Rank: {trailing_5_rank}", text_color(trailing_5_rank, reverse=True)),
+		]
+		annotations.append(
+		    create_annotation(
+			1.02,
+			1.10,
+			f"5d and 21d Concordance: {sign_agreement_5d}, {sign_agreement_21d}",
+			'white'
+		    )
+		)
+	# 	annotations.append(create_annotation(0.95, 1.12, f"Average 14-Period Rolling Correlation: {average_correlation}", 'white'))
+		fig.update_layout(
+		    title=f"Mean return path for {ticker2} in years {start}-present",
+		    legend=dict(
+			bgcolor='rgba(0,0,0,0)',
+			font=dict(color='White'),
+			itemclick='toggleothers',
+			itemdoubleclick='toggle',
+			traceorder='reversed',
+			orientation='h',
+			bordercolor='grey',
+			borderwidth=1,
+			x=-0.10,
+			y=-0.135 
+		    ),
+		    xaxis=dict(title='', color='white',showgrid=False),
+		    yaxis=dict(title='Mean Return', color='white',showgrid=False),
+		    font=dict(color='white'),
+		    margin=dict(l=40, r=40, t=40, b=70),  # Increase bottom margin
+		    hovermode='x',
+		    plot_bgcolor='Black',
+		    paper_bgcolor='Black',
+		    annotations=annotations  # Use the new annotations list with colored text
+		)
+		# Create a candlestick chart
+		fig2 = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.03)
 
-	fig2.add_trace(go.Scatter(x=df['date_str'], y=df['200_MA'], name='200_MA', line=dict(color='purple')), row=2, col=1)
+		fig2.add_trace(go.Scatter(x=df.index, y=df['RSI'], name='RSI'), row=1, col=1)
+		fig2.add_trace(go.Candlestick(x=df['date_str'],
+					      open=df['Open'],
+					      high=df['High'],
+					      low=df['Low'],
+					      close=df['Close'], name='Price'), row=2, col=1)
 
-	# Adjust the domain of y-axes for the height ratio
-	fig2['layout']['yaxis1'].update(domain=[0.875, 1])
-	fig2['layout']['yaxis2'].update(domain=[0, 0.875], showgrid=False)
+		fig2.add_trace(go.Scatter(x=df['date_str'], y=df['200_MA'], name='200_MA', line=dict(color='purple')), row=2, col=1)
 
-	# Add horizontal lines at RSI=70 and RSI=30
-	fig2.update_layout(
-	    shapes=[
-		dict(type='line', yref='y1', y0=70, y1=70, xref='x', x0=df.index[0], x1=df.index[-1], line=dict(color='Red')),
-		dict(type='line', yref='y1', y0=30, y1=30, xref='x', x0=df.index[0], x1=df.index[-1], line=dict(color='Green'))
-	    ],
-	    title=ticker, # Replace ticker with your ticker variable
-	    title_x=0.5, # This will center the title
-	    height=800, 
-	    width=1200
-	)
+		# Adjust the domain of y-axes for the height ratio
+		fig2['layout']['yaxis1'].update(domain=[0.875, 1])
+		fig2['layout']['yaxis2'].update(domain=[0, 0.875], showgrid=False)
 
-	fig2.update_xaxes(showgrid=False)
-	fig2.update_yaxes(showgrid=False)
+		# Add horizontal lines at RSI=70 and RSI=30
+		fig2.update_layout(
+		    shapes=[
+			dict(type='line', yref='y1', y0=70, y1=70, xref='x', x0=df.index[0], x1=df.index[-1], line=dict(color='Red')),
+			dict(type='line', yref='y1', y0=30, y1=30, xref='x', x0=df.index[0], x1=df.index[-1], line=dict(color='Green'))
+		    ],
+		    title=ticker, # Replace ticker with your ticker variable
+		    title_x=0.5, # This will center the title
+		    height=800, 
+		    width=1200
+		)
 
-	st.plotly_chart(fig)
-	st.plotly_chart(fig2)
+		fig2.update_xaxes(showgrid=False)
+		fig2.update_yaxes(showgrid=False)
+
+		st.plotly_chart(fig)
+		st.plotly_chart(fig2)
 
 # Download and parse the content of the text files from the GitHub repository
 base_url = "https://raw.githubusercontent.com/mslade50/New_seasonals/main/"
