b54d43c641c0726bbceebbcee769d0e4bd43cce9
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-05-16 07:48:38 -0400

    Update bear_signals.py

--- pages/bear_signals.py
+++ pages/bear_signals.py
@@ -498,23 +498,22 @@ def seasonals_chart(tick):
 		sign_agreement_21d = sign_agreement(s4_values, this_year_values, window=21).round(2)
 		# Add a white dot at the specified X coordinate and the interpolated Y value
 		fig.add_trace(go.Scatter(x=[length_value], y=[y_value_at_length], mode='markers', marker=dict(color='white', size=8), name='White Dot' ,showlegend=False))
-		    def text_color(value, reverse=False):
+		def text_color(value, reverse=False):
 			if not reverse:
-			    if value >= 85:
-				return 'green'
-			    elif value <= 15:
-				return 'red'
-			    else:
-				return 'white'
+				if value >= 85:
+					return 'green'
+				elif value <= 15:
+					return 'red'
+				else:
+					return 'white'
 			else:
-			    if value >= 85:
-				return 'red'
-			    elif value <= 15:
-				return 'green'
-			    else:
-				return 'white'
-
-		    def create_annotation(x, y, text, color):
+				if value >= 85:
+					return 'red'
+				elif value <= 15:
+					return 'green'
+				else:
+					return 'white'
+		def create_annotation(x, y, text, color):
 			return dict(
 			    x=x,
 			    y=y,
@@ -529,7 +528,6 @@ def seasonals_chart(tick):
 			    borderpad=4,
 			    align='left'
 			)
-
 			annotations = [
 			    create_annotation(0.4, -0.22, f"Cycle Avg: {cycle_avg}", text_color(cycle_avg)),
 			    create_annotation(0.55, -0.22, f"Total Avg: {total_avg}", text_color(total_avg)),
