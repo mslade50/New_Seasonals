3a3d1e2f801439b3fa17db942a6645073e3ec4fc
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-06-28 20:13:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+User
 import streamlit as st
-from pages import bull_signals, positions, bullish_seasonals, bearish_seasonals, user_input, filtered_bull, filtered_bear
+from pages import bull_signals, indicies, positions, single_names, currency_pairs, commodities, watchlist, bullish_seasonals, bearish_seasonals, user_input, filtered_bull, filtered_bear
 
 st.set_page_config(page_title="Multi-Page Dashboard", page_icon=":chart_with_upwards_trend:")
 
@@ -7,9 +8,29 @@ def bull_signals_page():
     st.sidebar.markdown("# Daily Signals")
     bull_signals.daily_signals_app()
 
+def indicies_page():
+    st.sidebar.markdown("# Indices")
+    indicies.indicies_app()
+
 def positions_page():
     st.sidebar.markdown("# Positions")
     positions.positions_app()
+
+def single_names_page():
+    st.sidebar.markdown("# Single Names")
+    single_names.single_names_app()
+
+def currency_pairs_page():
+    st.sidebar.markdown("# Currency Pairs")
+    currency_pairs.currency_pairs_app()
+
+def commodities_page():
+    st.sidebar.markdown("# Commodities")
+    commodities.commodities_app()
+
+def watchlist_page():
+    st.sidebar.markdown("# Watchlist")
+    watchlist.watchlist_app()
 
 def bullish_seasonals_page():
     st.sidebar.markdown("# Bullish Seasonals")
@@ -33,15 +54,16 @@ def filtered_bear_page():
 
 page_names_to_funcs = {
     "Daily Signals": bull_signals_page,
+    "Indices": indicies_page,
     "Positions": positions_page,
+    "Single Names": single_names_page,
+    "Currency Pairs": currency_pairs_page,
+    "Commodities": commodities_page,
+    "Watchlist": watchlist_page,
     "Bullish Seasonals": bullish_seasonals_page,
     "Bearish Seasonals": bearish_seasonals_page,
     "User Input": user_input_page,
-    "Filtered Bull": filtered_bull_page,
-    "Filtered Bear": filtered_bear_page,
+    "Filtered Bull": filtered_bull_page,  # New line
+    "Filtered Bear": filtered_bear_page,  # New line
 }
-
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
-
 
