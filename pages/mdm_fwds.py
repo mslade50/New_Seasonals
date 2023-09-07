4a7e475d0c0e0f0fb2f129775877416eda9f51ac
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-09-07 08:37:00 -0400

    Update mdm_fwds.py

--- pages/mdm_fwds.py
+++ pages/mdm_fwds.py
@@ -6,17 +6,24 @@ import streamlit as st
 import plotly.express as px
 
 st.title("Forward Ranks")
+
 def download_db():
+    st.write("Downloading database...")
     url = "https://github.com/mslade50/New_Seasonals/raw/main/ticker_data.db"
     r = requests.get(url)
     with open("ticker_data.db", 'wb') as f:
         f.write(r.content)
+    st.write("Database downloaded.")
 
 def app():
+    st.write("Inside app function.")
+    
     download_db()
-
+    
+    st.write("Trying to connect to database.")
     conn = sqlite3.connect("ticker_data.db")
 
+    st.write("Connected to database. Running query.")
     query = """
     SELECT *
     FROM ticker_results
@@ -26,9 +33,10 @@ def app():
         GROUP BY Ticker
     );
     """
-
     df = pd.read_sql_query(query, conn)
+    st.write("Query executed. Showing head of DataFrame.")
     st.write(df.head())
+    
     fig1 = px.bar(df, x='Ticker', y='F5', title='F5 Data')
     st.plotly_chart(fig1)
 
@@ -40,3 +48,4 @@ def app():
 
     fig4 = px.bar(df, x='Ticker', y='Vol_Change', title='Vol_Change Data')
     st.plotly_chart(fig4)
+
