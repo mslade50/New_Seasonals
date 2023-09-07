6e4e022bf5d806717712b5b29e621b07f261c583
mslade50 <107962250+mslade50@users.noreply.github.com>
2023-09-07 08:08:50 -0400

    Create mdm_fwds.py

---
+++ pages/mdm_fwds.py
@@ -0,0 +1,42 @@
+import requests
+import os
+import sqlite3
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+def download_db():
+    url = "https://github.com/mslade50/New_Seasonals/raw/main/ticker_data.db"
+    r = requests.get(url)
+    with open("ticker_data.db", 'wb') as f:
+        f.write(r.content)
+
+def app():
+    if not os.path.exists("ticker_data.db"):
+        download_db()
+
+    conn = sqlite3.connect("ticker_data.db")
+
+    query = """
+    SELECT *
+    FROM ticker_results
+    WHERE (Ticker, timestamp) IN (
+        SELECT Ticker, MAX(timestamp)
+        FROM ticker_results
+        GROUP BY Ticker
+    );
+    """
+
+    df = pd.read_sql_query(query, conn)
+    
+    fig1 = px.bar(df, x='Ticker', y='F5', title='F5 Data')
+    st.plotly_chart(fig1)
+
+    fig2 = px.bar(df, x='Ticker', y='F21', title='F21 Data')
+    st.plotly_chart(fig2)
+
+    fig3 = px.bar(df, x='Ticker', y='F63', title='F63 Data')
+    st.plotly_chart(fig3)
+
+    fig4 = px.bar(df, x='Ticker', y='Vol_Change', title='Vol_Change Data')
+    st.plotly_chart(fig4)
