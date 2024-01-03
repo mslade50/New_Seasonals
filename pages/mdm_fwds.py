81d33defc13603892f4b923f01c47ccd9a8266f4
mslade50 <107962250+mslade50@users.noreply.github.com>
2024-01-03 08:38:26 -0500

    Update mdm_fwds.py

--- pages/mdm_fwds.py
+++ pages/mdm_fwds.py
@@ -24,10 +24,7 @@ def app():
     conn = sqlite3.connect("ticker_data.db")
 
     st.write("Connected to database. Running query.")
-
-    # Get today's date in the format stored in the database
     today = datetime.datetime.now().strftime("%Y-%m-%d")
-
     query = f"""
     SELECT *
     FROM ticker_results
@@ -38,11 +35,10 @@ def app():
         GROUP BY Ticker
     );
     """
-
     df = pd.read_sql_query(query, conn)
     st.write("Query executed. Showing head of DataFrame.")
     st.write(df.head(30))
-    
+
     fig1 = px.bar(df, x='Ticker', y='F5', title='F5 Data')
     st.plotly_chart(fig1)
 
@@ -55,6 +51,5 @@ def app():
     fig4 = px.bar(df, x='Ticker', y='Vol_Change', title='Vol_Change Data')
     st.plotly_chart(fig4)
 
-# Call the app function to run the app
 app()
 
