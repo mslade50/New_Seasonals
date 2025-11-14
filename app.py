1f3867c4017f73bc5af0251223bc4c6e97ecc05b
mslade50 <107962250+mslade50@users.noreply.github.com>
2025-11-14 11:40:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
-from pages import user_input_2, variance_sim, trade_dashboard
+from pages import user_input, variance_sim, trade_dashboard
 
 st.set_page_config(page_title="Multi-Page Dashboard", page_icon=":chart_with_upwards_trend:")
